7c53ad4cfa125a2749ec04fcd7606cddec852792
unknown <ahmedomani2003@gmail.com>
2024-10-28 22:16:20 +0300

    Added Customer Class

---
+++ include/Customer.h
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+
+using namespace std ;
+
+class Customer
+{
+    public:
+        Customer() {}
+
+    protected:
+
+    private:
+};
+
+#endif // CUSTOMER_H
--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include "Customer.h"
 
 using namespace std;
 
 int main()
 {
     cout << "Welcome to Travel Managment System using OOP" << endl;
+    Customer *Omani = new Customer() ;
     return 0;
 }
